fc44bf7e0fb034c3a127ba9a4a1b57b0e609fa71
Amanda Shih <pandafulmanda@gmail.com>
2019-01-14 21:06:19 +0000

    updates to the seeding

--- seeders/20190112182145-seed-users.js
+++ seeders/20190112182145-seed-users.js
@@ -1,44 +1,27 @@
 'use strict';
-const csvParse = require('csv-parse')
 
-const path = require('path')
-const fs = require('fs')
-
+const readCSV = require('../utils/read-csv')
 const db = require('../models')
-
-function readMock(relativeFilePath) {
-  return new Promise(function (resolve, reject) {
-    let results = []
-
-    fs.createReadStream(path.join(__dirname, relativeFilePath))
-      .pipe(csvParse({ columns: true }))
-      .on('data', function (row) {
-        // delete the row id from the CSV file so that the
-        // database can autogenerate the id
-        delete row.id
-        results.push(row)
-      })
-      .on('end', function (){
-        resolve(results)
-      })
-      .on('error', reject)
-  })
-}
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-    return readMock('../data/mock-users.csv')
+    return readCSV('../data/mock-users.csv')
       .then(function (users) {
-        return queryInterface.bulkInsert('Users', users)
+        const usersWithoutIds = users.map(function(user) {
+          const userCopy = Object.assign({}, user)
+          delete userCopy.id
+          return userCopy
+        })
+
+        return queryInterface.bulkInsert('Users', usersWithoutIds)
       })
 
   },
 
   down: (queryInterface, Sequelize) => {
 
-
-    return readMock('../data/mock-users.csv')
+    return readCSV('../data/mock-users.csv')
       .then(function (users) {
         const mockUserEmails = users.map(function(user) {
           return user.email
